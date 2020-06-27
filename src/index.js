017b031ec096132d7571c143d79d7ec53c41b6d7
Humzabj <humza-b-jay@hotmail.com>
2020-06-27 11:44:15 +1000

    Most of the functionality is moved to the Game component. We are now tracking the moves. The board component now recieves props from the Game component

--- src/index.js
+++ src/index.js
@@ -50,45 +50,31 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    // Must call super constructor to reference props from parent with this keyword
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
+  /*
+  Constructor removed because now the Game component is handling the state and passing 
+  the props.
+  */
+  // constructor(props) {
+  //   // Must call super constructor to reference props from parent with this keyword
+  //   super(props);
+  //   this.state = {
+  //     squares: Array(9).fill(null),
+  //     xIsNext: true,
+  //   }
+  // }
 
   renderSquare(i) {
     return (
       <Square
-        value={ this.state.squares[i] }
-        onClick={() => { this.clickHandler(i)}}
+        value={ this.props.squares[i] }
+        onClick={() => { this.props.onClick(i)}}
       />
     );
   }
 
-  clickHandler(i) {
-    // Creating a shallow copy for immutability with slice() method
-    /* 
-      Immutability allows us to keep track of changes made to the data.
-      This can help in implementing functions that detect changes in data e.g. undo.
-      We can also use the manipulated data to trigger re-rendering as per the business 
-      logic (i.e pure components, look at shouldComponentUpdate() funciton).
-    */ 
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -110,15 +96,87 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    }
+  }
+
+  clickHandler(i) {
+    // Creating a shallow copy for immutability with slice() method
+    /* 
+      Immutability allows us to keep track of changes made to the data.
+      This can help in implementing functions that detect changes in data e.g. undo.
+      We can also use the manipulated data to trigger re-rendering as per the business 
+      logic (i.e pure components, look at shouldComponentUpdate() funciton).
+    */ 
+    const history = this.state.history;
+    // Current move will always be the last index of history
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    // The .concat() method does not mutate the original array like the .push() method does.
+    this.setState({
+      history: [{
+        squares: squares,
+      }],
+      stepNumber: 0,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    // xIsNext is set depending on the turn being even or odd
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2) === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    // Current move will always be the last index of history
+    const current = history[history.length - 1];
+    // Winner will be decided on current move
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+      'Go to move #' + move : 
+      'Go to game start';
+      return (
+        <li key={ move }>
+          <button onClick={() => this.jumpTo(move)}>{ desc }</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          {/* Passing props to board. The clickHandler is now in Game component. */}
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.clickHandler(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{ status }</div>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
@@ -131,6 +189,28 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// default angular getting started function
+// TODO: make your own algorithm.
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 
 // If you want your app to work offline and load faster, you can change
